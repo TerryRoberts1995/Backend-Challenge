dc8f7176d266e923e6471f8cc6026af6a435e824
Terry Roberts <terryjbroberts@yahoo.com>
2024-03-12 04:57:47 -0500

    testing port

--- server.js
+++ server.js
@@ -16,3 +16,8 @@ mongoose
   .then(() => {
     console.log("DB connection is successful!");
   });
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
